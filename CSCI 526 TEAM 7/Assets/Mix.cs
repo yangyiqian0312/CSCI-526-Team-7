82301afb609c65145bf14615ae11e82a16173bc5
Zhiyu Liao <zhiyuliao@Zhiyus-MBP.attlocal.net>
2022-02-07 20:45:46 -0800

    Added mixture steps

---
+++ CSCI 526 TEAM 7/Assets/Mix.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mix : MonoBehaviour
+{
+    public void newWindow(){
+        SceneManager.LoadScene("Bar");
+    }
+}
