4f976e0667ad929ca5c88e34ec653ab04f92e96d
Zhiyu Liao <zhiyuliao@Zhiyus-MBP.attlocal.net>
2022-02-07 20:57:31 -0800

    Fix the structure problem in the mix scene

--- CSCI 526 TEAM 7/Assets/Mix.cs
+++ CSCI 526 TEAM 7/Assets/Mix.cs
@@ -5,7 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class Mix : MonoBehaviour
 {
-    public void newWindow(){
+    public void backToBar(){
         SceneManager.LoadScene("Bar");
+    }
+
+    private int currChild = 0;
+
+    public void setChildBase(){
+        currChild = 0;
+    }
+
+    public void setChildModifier(){
+        currChild = 1;
+    }
+
+    public void setChildFlavoring(){
+        currChild = 2;
+    }
+
+    public void showChild(){
+        this.transform.GetChild(currChild).gameObject.SetActive(true);
     }
 }
