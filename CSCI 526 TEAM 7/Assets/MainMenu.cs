00b6fb02edc1c0c62a1ec1eba9db51aeb4df1267
Zhiyu Liao <zhiyuliao@Zhiyus-MBP.attlocal.net>
2022-02-24 22:40:57 -0800

    feat: Customer requirement compare added

--- CSCI 526 TEAM 7/Assets/MainMenu.cs
+++ CSCI 526 TEAM 7/Assets/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Analytics;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
@@ -16,6 +17,20 @@ public class MainMenu : MonoBehaviour
         {
             { "option_selected", optionSelected }
         });
+    }
+
+    void Awake(){
+
+        System.Random rnd = new System.Random();
+        int currID = rnd.Next(0, GameData.customerIDRange);
+        GameData.currCustomer = GameData.customers[currID];
+
+        GameObject.Find("CustomerDialogue").GetComponent<TMPro.TextMeshProUGUI>().text 
+            = GameData.currCustomer.dialogue;
+
+
+        GameObject.Find("CustomerName").GetComponent<TMPro.TextMeshProUGUI>().text 
+            = GameData.currCustomer.name;
     }
     
 }
