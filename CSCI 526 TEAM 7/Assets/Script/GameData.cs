2ea5eae9fb402a99a1390c9f8cb04c53075adcc6
wil-huang <whuang06@usc.edu>
2022-02-25 20:52:43 -0800

    develop skill panel prototype v1

---
+++ CSCI 526 TEAM 7/Assets/Script/GameData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameData
+{
+    public static float money = 10;
+    public static int upgradeTipsNeed = 2;
+    public static int tipsLevel = 0;
+}
---
+++ CSCI 526 TEAM 7/Assets/Script/SkillsPanel/TipsButton.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TipsButton : MonoBehaviour {
+    public Text prompt;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void upgradeTips() {
+        float currMoney = GameData.money;
+        int upgradeNeed = GameData.upgradeTipsNeed;
+        int tipsLevel = GameData.tipsLevel;
+        string tmp;
+
+        if (currMoney >= upgradeNeed) {
+            currMoney -= upgradeNeed;
+            tipsLevel++;
+            tmp = "-- PURCHASE COMPLETE --\n Remaining Money: " + currMoney;
+        } else {
+            tmp = "-- INVALID PURCHASE --\nYOU CAN'T AFFORD THAT";
+        }
+        // "Money Remaining:" + GameData.tips
+        GameData.money = currMoney;
+        GameData.tipsLevel = tipsLevel;
+        prompt.text = tmp;
+    }
+}
