6ab814bdfb92c5163ff4bcda85e6c47f6378f076
Zhiyu Liao <zhiyuliao@Zhiyus-MBP.attlocal.net>
2022-03-03 00:10:49 -0800

    feat: Basic tutorials

--- CSCI 526 TEAM 7/Assets/MainMenu.cs
+++ CSCI 526 TEAM 7/Assets/MainMenu.cs
@@ -7,25 +7,14 @@ using System;
 
 public class MainMenu : MonoBehaviour
 {
-    public void newWindow(){
-        SceneManager.LoadScene("Bar");
-    }
-
-    public void SendAnalyticsServeCustomer(string optionSelected)
-    {
-        Analytics.CustomEvent("serve_customer", new Dictionary<string, object>
-        {
-            { "option_selected", optionSelected }
-        });
-    }
-
     void Awake(){
         int currID;
-        if(GameData.dialogueTutorial == 0){
+        if(GameData.TutorialDone == 0){
             currID = 6;
-            GameData.dialogueTutorial = 1;
         }
         else{
+
+            GameObject.Find("DialogueTutorial").SetActive(false);
             System.Random rnd = new System.Random();
             currID = rnd.Next(0, GameData.customerIDRange);
         }
@@ -38,6 +27,18 @@ public class MainMenu : MonoBehaviour
 
         GameObject.Find("CustomerName").GetComponent<TMPro.TextMeshProUGUI>().text 
             = GameData.currCustomer.name;
+    }
+
+    public void newWindow(){
+        SceneManager.LoadScene("Bar");
+    }
+
+    public void SendAnalyticsServeCustomer(string optionSelected)
+    {
+        Analytics.CustomEvent("serve_customer", new Dictionary<string, object>
+        {
+            { "option_selected", optionSelected }
+        });
     }
     
 }
